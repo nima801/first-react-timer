5bd498d7d4873ab01dac3d9f0736a4c81bd651b3
Nima <nimaaligholizadeh49@gmail.com>
2025-08-04 05:07:46 +0330

    use timer like component

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom/client';
 
 // import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <h1>hello to All the world</h1>
-);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <h1>hello to All the world</h1>
+// );
 
 // test note for git push
 // this note is too!
+
+/*
 let time = ()=>{
   const elem= (
     <div>
@@ -23,8 +25,51 @@ let time = ()=>{
 }
 setInterval(time,1000)
 
+*/
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 
 // reportWebVitals();
+
+
+
+// new timer with class Type component
+
+
+class Timer extends React.Component{
+  render(){
+    return(
+      <h2>it is: {new Date().toLocaleTimeString()}</h2>
+    )
+  }
+}
+
+class Hello extends React.Component{
+  render(){
+    return(
+      <h1>Hello My Friends</h1>
+    )
+  }
+}
+
+class Atall extends React.Component{
+  render(){
+    return(
+      <>
+        <Hello/>
+        <Timer/>
+      </>
+    )
+  }
+}
+
+let elem = ReactDOM.createRoot(document.getElementById("root"))
+// let func = ()=>{
+//   elem.render(<Atall/>)
+// }
+
+setInterval(()=>{
+  elem.render(<Atall/>)
+},1000)
