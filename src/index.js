e38a03e9109784f30fd68abf4d3c5aee9229732b
Nima <nimaaligholizadeh49@gmail.com>
2025-08-04 04:14:34 +0330

    first react project like timer

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+// import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <h1>hello to All the world</h1>
 );
+
+let time = ()=>{
+  const elem= (
+    <div>
+      <h1>the local time</h1>
+      <h2>
+        the time is: {new Date().toLocaleTimeString()}
+      </h2>
+    </div>
+  )
+  ReactDOM.createRoot(document.getElementById("root")).render(elem);
+}
+setInterval(time,1000)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+// reportWebVitals();
